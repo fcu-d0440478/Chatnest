5347568d48c2664c2332c7da15be4009e42335fd
FantasyLin <yungyi.cool@gmail.com>
2025-03-17 22:44:32 +0800

    new

---
+++ src/main/java/com/eric/ChatNest/ChatNestApplication.java
@@ -0,0 +1,13 @@
+package com.eric.ChatNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eric/ChatNest/ChatNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.ChatNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
